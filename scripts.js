c7b99d9765401a9bea2b07e48a81cc80733f2a34
Byron <jamesblocker81@gmail.com>
2018-01-29 17:12:27 -0800

    add in randomNum function

--- scripts.js
+++ scripts.js
@@ -8,11 +8,22 @@ function tested() {
     console.log('tested');
 }
 
+function randomInt(n){
+    return Math.floor(Math.random() * n)
+}
+
+//buttons hooked up
 var redButton = document.querySelector('#redBtn');
 redButton.addEventListener('click', tested);
-
 var blueButton = document.querySelector('#blueBtn');
 blueButton.addEventListener('click', tested);
-
 var greenButton = document.querySelector('#greenBtn');
 greenButton.addEventListener('click', tested);
+
+var colorArr = ["Red", "Green", "Blue"];
+
+var displayWord = document.querySelector('#main-box')
+displayWord.innerText = colorArr[2]
+
+//button press
+var colorNum = randomInt(2);
